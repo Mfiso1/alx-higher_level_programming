541e77f7152e4011c085adcfc47d16e782cd9ade
Mfiso <mfsdmnomezulu@gmail.com>
2023-09-11 18:57:42 -0700

    element_at adjusted

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < len(my_list):
+    if 0 <= idx < len(my_list):
         return my_list[idx]
     else:
-        return None
+        return my_list
