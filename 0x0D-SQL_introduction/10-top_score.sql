c6d369f1a82e2d365b9c2a57d262af0151edf88f
Mfiso1 <mfsdmngomezulu@gmail.com>
2024-03-26 16:58:18 -0700

    File Edited

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
--- lists all records of the table second_table of the database hbtn_0c_0
+-- Lists all records of the table second_table of the database
 SELECT score, name
 FROM hbtn_0c_0.second_table
 ORDER BY score DESC;
