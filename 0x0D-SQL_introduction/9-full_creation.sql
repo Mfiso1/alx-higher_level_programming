3c8884279919384ea394139823cfd939c52e47fe
Mfiso1 <mfsdmngomezulu@gmail.com>
2024-03-26 16:48:48 -0700

    File Edited

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Creates a table second_table in the database hbtn_0c_0 add multiple rows
-CREATE TABLE second_table(
+CREATE TABLE IF NOT EXISTS second_table(
 	id INT
 	name VARCHAR(256)
 	score INT
