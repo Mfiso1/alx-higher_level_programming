82b11ab26db205f83135cd186145cb5857562536
Mfiso1 <mfsdmngomezulu@gmail.com>
2024-03-26 16:50:44 -0700

    File Edited

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- Creates a table second_table in the database hbtn_0c_0 add multiple rows
 CREATE TABLE IF NOT EXISTS second_table(
-	id INT
-	name VARCHAR(256)
+	id INT,
+	name VARCHAR(256),
 	score INT
 );
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
